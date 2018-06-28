374643b871290fa49172270b0aff9dbf3802d5c0
Jane Costa <yberdysh@gmail.com>
2018-06-27 20:42:39 -0400

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
 def my_all?(collection)
-
+	i = 0
+	keep_track = true
+	while i < collection.length
+		result = yield collection[i]
+		keep_track = result == true ? true : false
+		i += 1
+		break if keep_track == false
+	end
+	return collection
 end
